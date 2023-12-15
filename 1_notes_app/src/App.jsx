303498ffaf5d0e1cb7e138f3007fa9115d3fbbbc
ricky-kiva <ricky.kiva@gmail.com>
2023-12-15 10:16:17 +0700

    feat(src-app): impl. important Notes filter

--- 1_notes_app/src/App.jsx
+++ 1_notes_app/src/App.jsx
@@ -4,6 +4,7 @@ import Note from './components/Note'
 const  App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -20,13 +21,22 @@ const  App = (props) => {
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
+
+  const notesToShow = showAll 
+    ? notes 
+    : notes.filter(note => note.important)
   
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'Important' : 'All'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note, i) => 
-          <Note key={i} note={note} />
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
         )}
       </ul>
       <form onSubmit={addNote}>
