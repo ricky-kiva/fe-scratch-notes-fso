d861a77310dadadc816877a83015a638abf535d1
ricky-kiva <ricky.kiva@gmail.com>
2023-12-15 10:08:33 +0700

    feat(src-app): impl. Note submit func.

--- 1_notes_app/src/App.jsx
+++ 1_notes_app/src/App.jsx
@@ -1,14 +1,43 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const  App = ({ notes }) =>
-  <div>
-    <h1>Notes</h1>
-    <ul>
-      {notes.map((note, i) => 
-        <Note key={i} note={note} />
-      )}
-    </ul>
-  </div>
+const  App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
 
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+  
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note, i) => 
+          <Note key={i} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}  
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  )
+}
 
 export default App
