401915d5a6239d9dbcb6e09437bea0fc1a513b46
ricky-kiva <ricky.kiva@gmail.com>
2023-12-16 18:21:16 +0700

    feat(src): impl. basic error notification

--- 1_notes_app/src/App.jsx
+++ 1_notes_app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const effectHook = () => {
     noteService.getAll()
@@ -37,8 +39,8 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       )
       .catch(error => {
-        alert(`The note '${note.content} was already deleted from server'`)
-        console.log(id)
+        setErrorMessage(`Note ${note.content} was already removed from server`)
+        setTimeout(() => setErrorMessage(null), 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -54,6 +56,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'Important' : 'All'}
