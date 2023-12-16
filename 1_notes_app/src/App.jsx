4dd259d23745f9a7c67a8b494b4206cf4aa959d1
ricky-kiva <ricky.kiva@gmail.com>
2023-12-16 12:57:22 +0700

    feat(src): impl. toggle note importance

--- 1_notes_app/src/App.jsx
+++ 1_notes_app/src/App.jsx
@@ -20,11 +20,26 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios
+      .put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(n => n.id !== id ? n : response.data))
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -45,7 +60,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
--- 1_notes_app/src/components/Note.jsx
+++ 1_notes_app/src/components/Note.jsx
@@ -1,4 +1,13 @@
-const Note = ({ note }) =>
-  <li>{note.content}</li>
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
+
+  return (
+    <li>
+      <button onClick={toggleImportance}>{label}</button>
+      &nbsp;{note.content}
+    </li>
+  )
+}
 
 export default Note
